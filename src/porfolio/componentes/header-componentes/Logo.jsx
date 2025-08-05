f7876005eff174b2e4401306a72ae9a57f7eb6f0
Juan Paz <jpaz.dev94@gmail.com>
2025-08-04 22:24:55 -0300

    Cambios visuales en el header bottom y en la descripcion de Julian Paz

--- src/porfolio/componentes/header-componentes/Logo.jsx
+++ src/porfolio/componentes/header-componentes/Logo.jsx
@@ -4,7 +4,7 @@ export default function Logo () {
   return (
     <div className='logo-contenedor'>
       <h1>Julian Paz</h1>
-      <h2>Escritor Independiente</h2>
+      <h2>Escritor Freelance</h2>
     </div>
   )
 }
