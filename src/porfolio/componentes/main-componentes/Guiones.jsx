18754b7875b0e45f365070dcb8c5f40320a67935
Juan Paz <jpaz.dev94@gmail.com>
2025-08-06 00:24:31 -0300

    Cambios en la estructura de los guiones. Nota escrita por Julian Paz

--- src/porfolio/componentes/main-componentes/Guiones.jsx
+++ src/porfolio/componentes/main-componentes/Guiones.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable import/no-absolute-path */
-import { useState, useEffect, useRef } from 'react'
+import { useState } from 'react'
 // Iconos
 import youtube from './icons/utube.svg'
 
@@ -59,10 +59,7 @@ const proyectosContenido = [
     año: '2000',
     canalName: 'El Pana Resumen',
     linkCanal: 'https://www.youtube.com/channel/UC48SiD4b1enoizobezFeO9A',
-    link: 'https://www.youtube.com/embed/1NAQn9eaeqU?si=0VuOt_03wmvQOEeu',
-    html: `
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <b>Fuga nam eveniet nisi atque veritatis?</b> Dolorem explicabo, voluptatum doloribus placeat voluptates repellat quaerat perferendis, optio et eum enim asperiores quae excepturi.</p>
-    `
+    link: 'https://www.youtube.com/embed/1NAQn9eaeqU?si=0VuOt_03wmvQOEeu'
   },
   {
     id: 1,
@@ -71,10 +68,7 @@ const proyectosContenido = [
     año: '2017',
     canalName: 'El Pana Resumen',
     linkCanal: 'https://www.youtube.com/channel/UC48SiD4b1enoizobezFeO9A',
-    link: 'https://www.youtube.com/embed/kEJcXgrTfLs?si=MkncovvpJ7OirmWB',
-    html: `
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <b>Fuga nam eveniet nisi atque veritatis?</b> Dolorem explicabo, voluptatum doloribus placeat voluptates repellat quaerat perferendis, optio et eum enim asperiores quae excepturi.</p>
-    `
+    link: 'https://www.youtube.com/embed/kEJcXgrTfLs?si=MkncovvpJ7OirmWB'
   },
   {
     id: 2,
@@ -83,10 +77,7 @@ const proyectosContenido = [
     año: '2006',
     canalName: 'El Pana Resumen',
     linkCanal: 'https://www.youtube.com/channel/UC48SiD4b1enoizobezFeO9A',
-    link: 'https://www.youtube.com/embed/K8LdD-c_0wg?si=HoDE-YRuibjT55Er',
-    html: `
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <b>Fuga nam eveniet nisi atque veritatis?</b> Dolorem explicabo, voluptatum doloribus placeat voluptates repellat quaerat perferendis, optio et eum enim asperiores quae excepturi.</p>
-    `
+    link: 'https://www.youtube.com/embed/K8LdD-c_0wg?si=HoDE-YRuibjT55Er'
   },
   {
     id: 3,
@@ -95,10 +86,7 @@ const proyectosContenido = [
     año: '2012',
     canalName: 'El Pana Resumen',
     linkCanal: 'https://www.youtube.com/channel/UC48SiD4b1enoizobezFeO9A',
-    link: 'https://www.youtube.com/embed/bX4AxLKLXHQ?si=zeHz-g88S1SM1afD',
-    html: `
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <b>Fuga nam eveniet nisi atque veritatis?</b> Dolorem explicabo, voluptatum doloribus placeat voluptates repellat quaerat perferendis, optio et eum enim asperiores quae excepturi.</p>
-    `
+    link: 'https://www.youtube.com/embed/bX4AxLKLXHQ?si=zeHz-g88S1SM1afD'
   },
   {
     id: 4,
@@ -107,10 +95,7 @@ const proyectosContenido = [
     año: '2020',
     canalName: 'El Pana Resumen',
     linkCanal: 'https://www.youtube.com/channel/UC48SiD4b1enoizobezFeO9A',
-    link: 'https://www.youtube.com/embed/H3IaV69-OZ8?si=u_PMBRShQ4ZR62RA',
-    html: `
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <b>Fuga nam eveniet nisi atque veritatis?</b> Dolorem explicabo, voluptatum doloribus placeat voluptates repellat quaerat perferendis, optio et eum enim asperiores quae excepturi.</p>
-    `
+    link: 'https://www.youtube.com/embed/H3IaV69-OZ8?si=u_PMBRShQ4ZR62RA'
   },
   {
     id: 5,
@@ -119,10 +104,7 @@ const proyectosContenido = [
     año: '2020',
     canalName: 'El Pana Resumen',
     linkCanal: 'https://www.youtube.com/channel/UC48SiD4b1enoizobezFeO9A',
-    link: 'https://www.youtube.com/embed/2jGovmXA3tE?si=09JakJsUe01aV9VS',
-    html: `
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <b>Fuga nam eveniet nisi atque veritatis?</b> Dolorem explicabo, voluptatum doloribus placeat voluptates repellat quaerat perferendis, optio et eum enim asperiores quae excepturi.</p>
-    `
+    link: 'https://www.youtube.com/embed/2jGovmXA3tE?si=09JakJsUe01aV9VS'
   },
   {
     id: 6,
@@ -131,10 +113,7 @@ const proyectosContenido = [
     año: '2013',
     canalName: 'El Pana Resumen',
     linkCanal: 'https://www.youtube.com/channel/UC48SiD4b1enoizobezFeO9A',
-    link: 'https://www.youtube.com/embed/PSYRqTzZuRc?si=OZUr3GyzWo0BbdCt',
-    html: `
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <b>Fuga nam eveniet nisi atque veritatis?</b> Dolorem explicabo, voluptatum doloribus placeat voluptates repellat quaerat perferendis, optio et eum enim asperiores quae excepturi.</p>
-    `
+    link: 'https://www.youtube.com/embed/PSYRqTzZuRc?si=OZUr3GyzWo0BbdCt'
   },
   {
     id: 7,
@@ -143,24 +122,12 @@ const proyectosContenido = [
     año: '2006',
     canalName: 'El Pana Resumen',
     linkCanal: 'https://www.youtube.com/channel/UC48SiD4b1enoizobezFeO9A',
-    link: 'https://www.youtube.com/embed/7kJYCf0e6yU?si=k6B2aRXZS21wfpCc',
-    html: `
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <b>Fuga nam eveniet nisi atque veritatis?</b> Dolorem explicabo, voluptatum doloribus placeat voluptates repellat quaerat perferendis, optio et eum enim asperiores quae excepturi.</p>
-    `
+    link: 'https://www.youtube.com/embed/7kJYCf0e6yU?si=k6B2aRXZS21wfpCc'
   }
 ]
 
 function Proyectos ({ id }) {
   const [boton, setBoton] = useState(0)
-  const ref = useRef(null)
-
-  useEffect(() => {
-    if (!ref.current) {
-      console.log('Ref es :' + ref)
-    } else {
-      ref.current.innerHTML = proyectosContenido[boton].html
-    }
-  }, [boton])
 
   function handleButton (idx) {
     setBoton(idx)
@@ -171,6 +138,9 @@ function Proyectos ({ id }) {
       <div id={id} className='container-lg'>
         <div className='proyectos-encabezado'>
           <h1>Guiones</h1>
+        </div>
+        <div className='proyectos-nota'>
+          <p>Mis vídeos no solamente se dedican a contar en forma de cuentito la trama de las películas, de manera informal como si fuera una charla entre amigos, si no que también trato de señalar con sarcasmo e ironía los claros errores de guion, las actuaciones malas, las escenas clichés y cursis, los malos efectos especiales, etc., con tal de que los vídeos resulten más atrapantes.</p>
         </div>
         <div className='proyectos-contenido d-lg-flex flex-lg-row'>
           <div className='proyectos-botones'>
@@ -190,15 +160,8 @@ function Proyectos ({ id }) {
               <h2>{`${proyectosContenido[boton].titulo} (${proyectosContenido[boton].año})`}</h2>
               <p>Genero:{proyectosContenido[boton].genero} - Guion para <a href={proyectosContenido[boton].linkCanal} target='blank'> {proyectosContenido[boton].canalName} </a></p>
             </div>
-            <div className='proyectos-resumen'>
-              <div className='proyectos-img-container'>
-                <div className='proyectos-img'>
-                  <iframe src={proyectosContenido[boton].link} title='YouTube video player' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerPolicy='strict-origin-when-cross-origin' allowfullscreen />
-                </div>
-              </div>
-              <div className='proyectos-text'>
-                <div ref={ref} className='proyectos-parrafos' />
-              </div>
+            <div className='proyectos-videos'>
+              <iframe src={proyectosContenido[boton].link} title='YouTube video player' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerPolicy='strict-origin-when-cross-origin' allowfullscreen />
             </div>
           </div>
         </div>
